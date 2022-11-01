a082b5e2ee26f9c5ec918ec624488c5b767cb370
Harun <94783125+harunakcay@users.noreply.github.com>
2022-11-01 23:12:59 +0300

    Revert "Sign up page"

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/Team-41-Frontend/" : "/",
+  transpileDependencies: true
 })
