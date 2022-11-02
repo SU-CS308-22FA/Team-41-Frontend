07d31643c4894be510971006d2d52e97d9591cde
Harun Akçay <meteharun@sabanciuniv.edu>
2022-11-03 01:13:10 +0300

    editProfile

---
+++ src/components/EditProfile.vue
@@ -0,0 +1,176 @@
+<template>
+    <LoggedNavBar></LoggedNavBar>
+
+    <div class="container">
+    
+            
+        <div class="editp">      
+            <div class="info">
+                <h2 style="color: rgba(17, 73, 158, 0.818)">Edit Profile</h2>
+                <input type="text" name="" placeholder="Name">
+                <input type="email" name="" placeholder="Email">
+                <input type="password" name="" placeholder="Current password">
+                <input type="password" name="" placeholder="New password">
+                <input type="password" name="" placeholder="New password (again)">
+                <button style="float:left;">CANCEL</button>
+                <button style="float:left; margin-left: 4%;">DONE</button>
+                
+            </div>
+            <div class="photo">
+                <img src="../assets/pp.jpg" alt="" class="center">
+                <br><br>
+                <input type="file" name="" id="file" accept="image/*">
+                <label for="file">Edit Photo</label>
+                <br><br><br><br>
+                <button style="float:left;height: 50px; width: 105px;;
+                 margin-left: 65%; background-color: #8B0000;">DELETE ACCOUNT</button>
+            </div>
+        </div>
+            
+            
+
+    </div>
+</template>
+
+<script>
+    import LoggedNavBar from './LoggedNavBar.vue'
+
+    export default {
+        path: '/EditProfile',
+        name: 'EditProfile',
+        components: {
+            LoggedNavBar,
+        },
+    };
+</script>
+
+<style scoped>
+    head{
+        box-sizing: border-box;
+        padding:0;
+        margin:0;
+    }
+
+    body{
+        margin: 0;
+        height: 100%;
+        align-items: center;
+        justify-content: center;
+    }
+    h2{
+        margin-left: -70%;
+    }
+    
+    .container{
+        background-image: linear-gradient(to bottom, rgba(145,146,122,0.62),rgba(47, 20, 109, 0.8)),url(../assets/ball.jpg);
+        background-size: cover;
+        height:100%;
+        margin: 3.2% -1% 0 -1%;
+        
+        display: flex;
+        padding-left: 33%;
+        
+    }
+
+    img{
+        box-sizing: border-box;
+
+        width: 149px;
+        height: 149px;
+        border-radius: 50%;
+        margin-bottom: -50%;
+
+        border:5px solid rgba(31, 28, 101, 0.818);
+        padding:3px;
+        background-color: white;
+        
+    }
+    .editp{
+        background-color: ghostwhite;
+        width: 450px;
+        height: 550px;
+        padding:40px;
+        margin: 1.1em 1em;
+        display: flex;
+        border-radius: 8px;
+        text-align: center;
+    }
+    .center {
+       
+        margin-left: 60%;
+        margin-top: 5%;
+        margin-bottom: 20%;
+        width: 100%;
+    }
+
+    input[type="text"],
+    input[type="password"],
+    input[type="email"]{
+        display:block;
+        box-sizing: border-box;
+        background: none;
+        margin-bottom: 40px;
+        padding:4px;
+        width:220px;
+        height:32px;
+        border:none;
+        border-bottom: 1px solid;
+        font-weight: 400;
+        font-size: 15px;
+        transition: 0.2;
+
+    }
+    h2{
+        margin-left: -40%;
+        margin-top: 5%;
+        margin-bottom: 20%;
+    }
+
+    input[type="text"]:focus,
+    input[type="password"]:focus,
+    input[type="email"]:focus{
+        border-bottom: 2px solid;
+        border-bottom-right-radius: 20px;
+        transition: 0.2 ease;
+    }
+
+    button{
+        border:1px solid;
+        background-color: rgba(17, 73, 158, 0.818);
+        color:white;
+        height: 30px;
+        width: 105px;
+        border-radius: 5px;
+        margin:0px;
+        transition:all 0.3s;
+    }
+
+    button:hover{
+        transform: scale(1.1);
+    }
+
+    input[type="file"]{
+        display:none;
+    }
+
+    label{
+        display:block;
+        box-sizing: border-box;
+        font-size: 20px;
+        background-color: rgba(17, 73, 158, 0.818);
+        color:white;
+        border:1px ;
+        padding: 3px 0px;
+        border-radius:5px;
+        transition: all 0.3s;
+    
+        margin-left: 30%;
+    }
+    label:hover{
+        background-color: black;
+        color:white;
+    }
+
+
+
+</style>
