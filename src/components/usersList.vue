436e9f196e06cef59e5f2600e981535865b88eee
Harun Akçay <meteharun@sabanciuniv.edu>
2022-12-14 19:50:59 +0300

    Create the users page for admins
    `
    Admins can see the whole list of users. Add name, email, birthdate,
    gender and favorite team information of users to the list. Add a button
    next to each line so that admins can ban the users they want. Banned
    users are listed on bottom of the page.

--- src/components/usersList.vue
+++ src/components/usersList.vue
@@ -4,12 +4,12 @@
     <SideBar></SideBar>
 
     <div class="whole">
-        <div class="standings">
+        <div class="users">
             <div v-if="users.length === 0 || finishedLoading === false">
                 <loadingPage></loadingPage>
             </div>
             <div >
-                <div class="headings">
+                <div class="headingss">
                     <table >
                         <tr class="col">
                             <th>ID</th>
@@ -19,7 +19,7 @@
                             <th>Birthdate</th>
                             <th>Fanteam</th>
                         </tr>
-                        <tr class= "uefa" v-for="user in users" :key="user.userId">
+                        <tr class= "userx" v-for="user in users" :key="user.userId">
                             <td>{{user.userId}}</td>
                             <td>{{user.name}}</td>
                             <td>{{user.mail}}</td>
@@ -27,7 +27,7 @@
                             <td>{{user.birthdate}}</td>
                             <td>{{user.fanTeam}}</td>
                             <td>
-                                <button @click="banUser(user.userId)">deactivate</button>
+                                <button class="ban-btn" @click="banUser(user.userId)">BAN</button>
                             </td>
                         </tr>
                     </table>
@@ -96,156 +96,67 @@
 
 <style>
 
-.standings{
+.users{
     display: flex;
     margin: auto;
 }
 
 
-.standings th{
+.users th{
     
     padding: 10px;
     font-weight: bold;
 }
-.standings td{
-    padding: 30px;
+.users td{
+    padding: 20px;
     font-weight: bold;
     
+    
 }
-.headings{
+.headingss{
 	text-align: center;
 	text-decoration: none;
 	color: black;
 	margin: 30px;
+    
 	display: block;
     background: ghostwhite;
 }
 
 
-
-.pos {
+.userx {
     color: black;
-    background: ghostwhite;
-    border-bottom: 2px solid black;
-}
-.pos .posrank{
-    color:black;
-    background: grey;
-}
-.pos:hover{
-	background-color: grey;
-}
-
-.pos:hover td{
-	color: #000000;
-    
-}
-
-.down {
-    color: red;
-    background: ghostwhite;
-    border-bottom: 2px solid black;
-}
-.cl{
-    color: rgb(50,205,50);
-    border-bottom: 2px solid black;
-}
-.cl .clrank{
-    background: rgb(50,205,50);
-    
-    color: black;
-}
-.cl:hover{
-	background-color: rgb(50,205,50);
-    
-}
-.cl:hover::after{
-    transition: width 0.3s ease;
-}
-
-.cl:hover td{
-	color: #000000;
-    
-}
-
-.down:hover{
-	background-color: red;
-}
-
-.uefa {
-    color: rgb(32,178,170);
     background: ghostwhite;
     border-bottom: 2px solid black;;
 }
-.uefa .uefarank{
+.userx .uefarank{
     background-color: rgb(32,178,170);
     color: black;
 }
-.uefa:hover{
-	background-color: rgb(32,178,170);
+
+
+.userx:hover td{
+	color: red;
+    background: rgb(208, 202, 202);
 }
 
-.uefa:hover td{
-	color: #000000;
-    
-}
-
-.down:hover td{
-	color: #000000;
-    
-}
-.down .downrank{
-    background: red;
-    color: black;
-}
-.grid-container {
-  display: grid;
-  grid-template-columns: 5% 95%;
-  width: 50%;
-  padding: 10px 10px 10px 10px;
-  
-}
-.grid-item {
-  color: black;
-  
-  padding-left: 5%;
-  font-size: 20px;
-  text-align: left;
-
-}
-.grid-item-cl {
-  background-color: rgb(50,205,50);
-  border: 1px solid rgba(0, 0, 0, 0.8);
-  margin-bottom: 10%;
-  margin-right: 5%;
-  margin-top: 10%;
-  font-size: 30px;
-  text-align: center;
-}
-.grid-item-uefa {
-  
-  border: 1px solid rgba(0, 0, 0, 0.8);
-  background-color: rgb(32,178,170);
-  font-size: 30px;
-  text-align: center;
-  margin-bottom: 10%;
-  margin-right: 5%;
-  margin-top: 10%;
-}
-.grid-item-down {
-  background-color: red;
-  border: 1px solid rgba(0, 0, 0, 0.8);
-  margin-bottom: 10%;
-  margin-right: 5%;
-  font-size: 30px;
-  margin-top: 10%;
-  text-align: center;
-}
 .col th{
     padding-top: 1%;
     padding-bottom: 2%;
     padding-left: 1%;
     padding-right:1%;
+}
+
+.ban-btn{
+    background: red;
+    color: white;
+    width: 80px;
+    height: 40px;
+
+}
+.ban-btn:hover{
+    cursor: pointer;
+    transform: scale(1.2);
 }
 
 
