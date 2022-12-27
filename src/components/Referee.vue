a2f71afde1754bd2f95407fcc2a1702da826b04d
atahf <ataollah@sabanciuniv.edu>
2022-12-27 17:11:11 +0300

    restyled refereeList

--- src/components/Referee.vue
+++ src/components/Referee.vue
@@ -1,40 +1,37 @@
 <template>
     <NavBar></NavBar>
-    <div class="whole">
-        
-        <div class="container">
-            <div v-if="items.length === 0 && finishedLoading === true">
-                No Matches Found!
-            </div>
-            <div v-else-if="items.length === 0 || finishedLoading === false">
-                <loadingPage></loadingPage>
-            </div>
-            <div v-else>
-                <div v-for="item in items[0]" :key="item.id">
-                    <router-link class="referee"  :to="{
+    <div class="referees-list">
+        <div v-if="items.length === 0 && finishedLoading === true">
+            No Referees Found!
+        </div>
+        <div v-else-if="items.length === 0 || finishedLoading === false">
+            <loadingPage></loadingPage>
+        </div>
+        <div v-else>
+            <div class="row row-cols-5 g-3">
+                <div class="col" v-for="item in items" :key="item.id">
+                    <div id="{{ item.id }}" class="card referee">
+                        <router-link :to="{
                             name: 'RefereePage',
                             params: {
                                 refereeId: item.id
                             }
-                        }"
-                    >
-                
-                    <div class="name">
-                        {{item.name}}
+                        }">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <a class="referee-name">{{item.name}}</a>
+                                </h5>
+                                <br>
+                                <a class="card-text referee-stats">Rating: &nbsp; {{item.rating}}</a>
+                                <br>
+                                <a class="card-text referee-stats">Total Votes: &nbsp; {{item.totalVote}}</a>
+                            </div>
+                        </router-link>
                     </div>
-                    
-                    <div class="name">
-                        Rating <br> {{item.rating}}
-                    </div>
-                    <div class="name">
-                        Total Votes <br> {{item.totalVote}}
-                    </div>
-                    </router-link>
                 </div>
             </div>
         </div>
     </div>
-
 </template>
 
 
@@ -52,7 +49,7 @@
         data(){
             return {
                 items : [],
-                isLoading : false
+                finishedLoading: false,
             }
         },
         mounted(){
@@ -64,7 +61,8 @@
             .then(response => response.json())
             .then(data => {
                 if(data.status === "200") {
-                    this.items.push(data.returnObject);
+                    let sortedData = data.returnObject.sort((i1, i2) => (i1.rating < i2.rating) ? 1 : (i1.rating > i2.rating) ? -1 : 0);
+                    this.items = sortedData;
                 }
                 this.finishedLoading = true;
             });
@@ -74,48 +72,51 @@
 
 </script>
 
-<style>
+<style scoped>
 
-.whole{
-    display: flex;
-}
-.container{
-    width: 39%;
-    margin-left: 10%;
-}
+    .referees-list{
+        margin-top: 70px;
+        padding: 25px 25px 25px 25px;
+        display: flex;
+        overflow: hidden;
+        
+        background: rgba(185, 185, 185, 0.725);
+    }
 
-.referee{
-    text-decoration: none;
-    display: flex;
-    justify-content: space-between;
-    padding: 10px 16px;
-    margin: 20px;
-    border: 1px solid black;
-    border-radius: 10px;
-    background-color: white;
-}
+    .referee{
+        justify-content: space-between;
+        padding: 10px 16px;
+        margin: 10px;
+        border: 1px solid black;
+        border-radius: 10px;
+        background-color: white;
+    }
 
+    .referee:hover{
+        border: 3px solid rgba(218, 38, 152, 0.978);
+        background-color: rgba(33, 66, 114, 0.818);
+        transform: scale(1.1);
+    }
 
-.referee:hover{
-    border: 3px solid rgba(218, 38, 152, 0.978);
-    background-color: rgba(33, 66, 114, 0.818);
-    color: white;
-    transform: scale(1.1);
+    .referee-name{
+        color: black;
+        font-weight: bold;
+        font-size: 21px;
+    }
 
-}
+    .referee-stats{
+        color: black;
+        font-weight: bold;
+        font-style: italic;
+        font-size: 17px;
+    }
 
+    .referee-name:hover, .referee-stats:hover{
+        color: white;
+    }
 
-.name{
-    color: black;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-weight: bold;
-}
-.referee .name:hover{
-    color: white;
-}
-
-
+    a{
+        text-decoration: none;
+    }
 
 </style>
