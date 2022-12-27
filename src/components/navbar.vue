6fe0961a3171f331d3b7d2ee3d14d2e55105d41f
atahf <ataollah@sabanciuniv.edu>
2022-12-27 16:46:29 +0300

    bug fixes in navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -8,43 +8,43 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 &nbsp;&nbsp;&nbsp;
                 <li class="nav-item active">
-                    <a class="nav-link active" aria-current="page" @click="this.$router.replace('/')">
+                    <a class="nav-link active right-buts" aria-current="page" @click="this.$router.replace('/')">
                         Home
                     </a>
                 </li>
                 &nbsp;&nbsp;&nbsp;
                 <li class="nav-item active">
-                    <a class="nav-link" @click="this.$router.replace('/todaysMatches')">
+                    <a class="nav-link active right-buts" @click="this.$router.replace('/todaysMatches')">
                         Todays's Matches
                     </a>
                 </li>
                 &nbsp;&nbsp;&nbsp;
                 <li class="nav-item active">
-                    <a class="nav-link" @click="this.$router.replace('/fixturePage')">
+                    <a class="nav-link active right-buts" @click="this.$router.replace('/fixturePage')">
                         Fixture
                     </a>
                 </li>
                 &nbsp;&nbsp;&nbsp;
                 <li class="nav-item active">
-                    <a class="nav-link" @click="this.$router.replace('/teamsList')">
+                    <a class="nav-link active right-buts" @click="this.$router.replace('/teamsList')">
                         Teams
                     </a>
                 </li>
                 &nbsp;&nbsp;&nbsp;
                 <li class="nav-item active">
-                    <a class="nav-link" @click="this.$router.replace('/referees')">
+                    <a class="nav-link active right-buts" @click="this.$router.replace('/referees')">
                         Referees
                     </a>
                 </li>
                 &nbsp;&nbsp;&nbsp;
                 <li class="nav-item active">
-                    <a class="nav-link" @click="this.$router.replace('/standings')">
+                    <a class="nav-link active right-buts" @click="this.$router.replace('/standings')">
                         Standings
                     </a>
                 </li>
                 &nbsp;&nbsp;&nbsp;
                 <li class="nav-item active" v-if="isLoggedIn()">
-                    <a class="nav-link" @click="this.$router.replace('/favoriteTeams')">
+                    <a class="nav-link active right-buts" @click="this.$router.replace('/favoriteTeams')">
                         Favorites
                     </a>
                 </li>
@@ -59,14 +59,14 @@
                 </li>
             </ul>
 
-            <a class="nav-link" v-if="isLoggedIn()" @click="this.$router.replace('/profilePage')">
+            <a class="nav-link active left-buts" v-if="isLoggedIn()" @click="this.$router.replace('/profilePage')">
                 Profile
             </a>
             &nbsp;&nbsp;&nbsp;
-            <a class="nav-link" v-if="isLoggedIn()" @click="logOut()">
+            <a class="nav-link active left-buts" v-if="isLoggedIn()" @click="logOut()">
                 Log Out
             </a>
-            <a class="nav-link" v-else @click="this.$router.replace('/login')">
+            <a class="nav-link active left-buts" v-else @click="this.$router.replace('/login')">
                 Log In
             </a>
             &nbsp;&nbsp;&nbsp;
@@ -106,11 +106,19 @@
         width: 175px;
     }
 
-    li:hover{
+    .left-buts, .right-buts{
+        color: black;
+        font-weight: 500;
+        border-radius: 10px;
+        padding: 8px;
+    }
+
+    .left-buts:hover, .right-buts:hover{
         background-color: white;
         color: black;
         font-weight: bold;
         transform: scale(1.1);
         border-radius: 10px;
+        padding: 8px;
     }
 </style>
