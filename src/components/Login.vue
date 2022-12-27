2578f42d4690f4e916945c150ce877e21fc77704
atahf <ataollah@sabanciuniv.edu>
2022-12-27 23:22:17 +0300

    restyled login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,41 +1,55 @@
 <template>
+    <!-- Modal -->
+    <div>
+        <div id="myModal" ref="modal" class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Error</h5>
+            </div>
+            <div class="modal-body">
+                <p>{{ errorMsg }}</p>
+            </div>
+            <div class="modal-footer">
+                <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+                @click="closeModal()"
+                >
+                Close
+                </button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+
     <NavBar></NavBar>
-    <div class="container">
+    <div class="login-page">
         <form class="login-form" @submit.prevent="login">
-            <h2 style="color: rgba(17, 73, 158, 0.818)">Login</h2>
+            <h2 class="title-login card-title" style="color: rgba(17, 73, 158, 0.818)"><b>Login</b></h2>
             <br>
-
-            <label for="mail">Mail</label>
-            <input name="mail" v-model="mail" placeholder="Enter mail" type="text" pattern="[^@]+@[^\.]+\..+" title="Invalid email address" required on-error="Invalid email address">
-
-            <label for="password">Password</label>
+            <br>
+            <input name="mail" v-model="mail" placeholder="Enter mail" type="text" pattern="[^@]+@[^\.]+\..+" title="Invalid email address" required autocomplete="current-username" on-error="Invalid email address">
+            <br>
             <input name="password" v-model="password" placeholder="Enter password" type="password" minlength="8" required autocomplete="current-password">
-
-            <button class="login">
+            <router-link class= "extra-links" to="#">Forget Password?</router-link>
+            <br>
+            <button class="but">
                 <a>Login</a>
             </button>
-
-            <div class="error">
-                <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
-                {{errorMsg}}
-            </div>
-            <br>
-            <div class="admin">
-                <div><p><a>Login as admin</a></p></div>
-            </div>
-            <br>
-            <div class="forget">
-                <div><p>Forget Password?<br> Click <a> here</a></p></div>
-            </div>
-            <br>
-            <div class="acc">
-                <div><p>Don't have an account?<br> Click <router-link class= "navborder" to="/signup">here</router-link></p></div>
-            </div>
+            <br><br><br>
+            <p>
+                Don't have an account?&nbsp;
+                <router-link class= "extra-links" to="/signup">Sign Up</router-link>
+            </p>
         </form>
     </div>
 </template>
 
 <script>
+    import { Modal } from "bootstrap";
     import NavBar from './navbar.vue'
 
     export default {
@@ -51,14 +65,20 @@
                 isLoged: false,
                 userId: "",
                 errorMsg: "",
+                myModal: null,
             };
         },
+        mounted() {
+            this.myModal = new Modal(document.getElementById("myModal"));
+        },
         methods: {
+            closeModal() {
+                this.myModal.hide();
+            },
+
             login() {
                 const { mail, password } = this;
 
-                var x = document.querySelector(".error");
-                x.style.display = "none";
                 this.errorMsg = "";
 
                 localStorage.userError = "";
@@ -82,16 +102,14 @@
                         } else {
                             this.errorMsg = res2.status;
                         }
-                        if (x.style.display === "none") {
-                            x.style.display = "block";
-                        }
-                        return;
+                        this.myModal.show();
                     }
-                    console.log(res2.returnObject);
-                    window.localStorage.setItem("isLogedIn", true);
-                    window.localStorage.setItem("userId", res2.returnObject.id);
-                    window.localStorage.setItem("isAdmin", res2.returnObject.admin);
-                    this.$router.push("/profilepage");// user profile sayfasina yonlendirilecek.
+                    else {
+                        window.localStorage.setItem("isLogedIn", true);
+                        window.localStorage.setItem("userId", res2.returnObject.id);
+                        window.localStorage.setItem("isAdmin", res2.returnObject.admin);
+                        this.$router.push("/profilepage");// user profile sayfasina yonlendirilecek.
+                    }
                 });
             }
         },
@@ -100,176 +118,56 @@
 
 
 <style scoped>
-    .container{
-        background-size: cover;
-        height:100%;
-        width: 100%;
-        text-align: center;
+    .title-login{
+        color: rgba(33, 66, 114, 0.818);
+    }
+
+    .login-page{
+        margin-top: 70px;
+        padding: 25px 25px 25px 25px;
         display: flex;
-        align-items: center;
-        padding-left: 60%;
-        margin-left: 0%;
-        margin-bottom: 0%;
+        overflow: hidden;
+        display: flexbox;
+        min-height: calc(100vh - 70px);
+        background: rgba(185, 185, 185, 0.725);
     }
 
     .login-form{
-        background-color: rgb(138, 138, 204);
-        width: 55%;
-        height: 100%;
+        background-color: ghostwhite;
+        width: auto;
+        height: fit-content;
         padding:30px;
-        margin: 1.1em 1em;
+        margin: auto;
         display:grid;
         border-radius: 8px;
-        text-align: left;
-
-        /*background: #2980b9 url('https://static.tumblr.com/03fbbc566b081016810402488936fbae/pqpk3dn/MRSmlzpj3/tumblr_static_bg3.png') repeat 0 0;
-        -webkit-animation: 10s linear 0s normal none infinite animate;
-        -moz-animation: 10s linear 0s normal none infinite animate;
-        -ms-animation: 10s linear 0s normal none infinite animate;
-        -o-animation: 10s linear 0s normal none infinite animate;
-        animation: 10s linear 0s normal none infinite animate;*/
-    }
-    
-    @-webkit-keyframes animate {
-        from {background-position:0 0;}
-        to {background-position: 500px 0;}
-    }
-    
-    @-moz-keyframes animate {
-        from {background-position:0 0;}
-        to {background-position: 500px 0;}
-    }
-    
-    @-ms-keyframes animate {
-        from {background-position:0 0;}
-        to {background-position: 500px 0;}
-    }
-    
-    @-o-keyframes animate {
-        from {background-position:0 0;}
-        to {background-position: 500px 0;}
-    }
-    
-    @keyframes animate {
-        from {background-position:0 0;}
-        to {background-position: 500px 0;}
+        text-align: center;
     }
 
-    input[type=text]{
-        margin-bottom: 20px;
-        border-radius: 7px;
-        padding:6px;
-        margin-top: 3px;
-    }
-    input[type=email]{
-        margin-bottom: 20px;
-        border-radius: 7px;
-        padding:6px;
-        margin-top: 3px;
-    }
-    input[type=password]{
-        margin-bottom: 20px;
-        border-radius: 7px;
-        padding:6px;
-        margin-top: 3px;
-    }
-
-    .login{
-        background-color: rgba(17, 73, 158, 0.818);
-        padding:10px;
-        border:none;
-        margin-top: 30px;
-        margin-bottom: 30px;
-        border-radius: 5px;
-        transition: 0.5s ease;
-    }
-
-    .login a{
+    .but{
+        margin: 20px 20px 20px 20px;
+        width: 210px;
+        border-radius: 8px;
+        background-color: rgba(33, 66, 114, 0.818);
+        border: 2px solid rgba(33, 66, 114, 0.818);
         color: white;
-        text-decoration: none;
-        font-weight: bold;
-        text-transform: uppercase;
-        font-size: medium;
+        font-size: large;
     }
-    .login:hover{
-        cursor: pointer;
+
+    .but:hover{
         transform: scale(1.1);
     }
-    
 
-    .forget{
-        display:flex;
-        align-items: center;
-        justify-content: space-between;
-    }
-
-    .forget a {
-        color:rgba(17, 73, 158, 0.818);
+    .extra-links{
         text-decoration: none;
-        transition: 0.1s ease;
-    }
-
-    .forget a:hover{
-        color:rgb(5, 5, 74);
-        cursor: pointer;
-    }
-
-    .acc{
-        display:flex;
-        align-items: center;
-        justify-content: space-between;
-    }
-
-    .acc a {
-        color:rgba(17, 73, 158, 0.818);
-        text-decoration: none;
-        transition: 0.1s ease;
-    }
-
-    .acc a:hover{
-        color:rgb(5, 5, 74);
-    }
-
-    .admin{
-        display:flex;
-        align-items: center;
-        justify-content: space-between;
-    }
-
-    .admin a {
-        color:rgba(17, 73, 158, 0.818);
-        text-decoration: none;
-        font-weight: bold;
-        transition: 0.1s ease;
-    }
-
-    .admin a:hover{
-        color:rgb(5, 5, 74);
-        cursor: pointer;
-    }
-
-    .error {
-        display: none;
-        padding: 20px;
-        background-color: #f44336; /* Red */
-        color: white;
-        margin-bottom: 15px;
-    }
-
-        /* The close button */
-    .closebtn {
-        margin-left: 15px;
-        color: white;
-        font-weight: bold;
-        float: right;
-        font-size: 22px;
-        line-height: 20px;
-        cursor: pointer;
-        transition: 0.3s;
-    }
-
-        /* When moving the mouse over the close button */
-    .closebtn:hover {
         color: black;
+        width: fit-content;
+        text-align: center;
+        margin: auto;
+        padding-top: 10px;
+    }
+
+    .extra-links:hover{
+        font-weight: bold;
+        font-style: italic;
     }
 </style>
