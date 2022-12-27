b3722c0655921f22b713cd45f6c8f8ab91fad4b4
atahf <ataollah@sabanciuniv.edu>
2022-12-27 16:56:35 +0300

    restyled teamList

--- src/components/TeamsList.vue
+++ src/components/TeamsList.vue
@@ -1,30 +1,29 @@
 <template>
     <NavBar></NavBar>
-    <div class="whole">
-        
-        <div class="container">
-            <div v-if="items.length === 0 && finishedLoading === true">
-                No Teams Found!
-            </div>
-            <div v-else-if="items.length === 0 || finishedLoading === false">
-                <loadingPage></loadingPage>
-            </div>
-            <div v-else>
-                <div class="row row-cols-2 g-3">
-                    <div class="col" v-for="item in items" :key="item.id">
-                        <div id="{{ item.id }}" class="card">
-                            <router-link :to="{
-                                name: 'TeamPage',
-                                params: {
-                                    teamId: item.id
-                                }
-                            }">
-                                <img :src=item.logoURL class="img-thumbnail">
-                                <div class="card-body">
-                                    <h5 class="card-title"><a>{{item.name}}</a></h5>
-                                </div>
-                            </router-link>
-                        </div>
+    <div class="teams-list">
+        <div v-if="items.length === 0 && finishedLoading === true">
+            No Teams Found!
+        </div>
+        <div v-else-if="items.length === 0 || finishedLoading === false">
+            <loadingPage></loadingPage>
+        </div>
+        <div v-else>
+            <div class="row row-cols-3 g-3">
+                <div class="col" v-for="item in items" :key="item.id">
+                    <div id="{{ item.id }}" class="card team">
+                        <router-link :to="{
+                            name: 'TeamPage',
+                            params: {
+                                teamId: item.id
+                            }
+                        }">
+                            <img :src=item.logoURL class="img-thumbnail team-logo">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <a class="team-name">{{item.name}}</a>
+                                </h5>
+                            </div>
+                        </router-link>
                     </div>
                 </div>
             </div>
@@ -70,43 +69,44 @@
 
 <style scoped>
 
-    .whole{
-        padding-top: 2.5%;
+    .teams-list{
+        margin-top: 70px;
+        padding: 25px 25px 25px 25px;
         display: flex;
+        overflow: hidden;
+        
         background: rgba(185, 185, 185, 0.725);
     }
 
-    .card{
+    .team{
         justify-content: space-between;
         padding: 10px 16px;
         margin: 20px;
         border: 1px solid black;
         border-radius: 10px;
         background-color: white;
-        color: black;
-        text-decoration: none;
     }
 
-    .card:hover{
+    .team:hover{
         border: 3px solid rgba(218, 38, 152, 0.978);
         background-color: rgba(33, 66, 114, 0.818);
-        color: white;
         transform: scale(1.1);
-        text-decoration: none;
+    }
+
+    .team-name{
+        color: black;
+    }
+
+    .team-name:hover{
+        color: white;
+    }
+
+    .team-logo{
+        border-width: 0px;
     }
 
     a{
         text-decoration: none;
-        color: black;
-    }
-
-    a:hover{
-        text-decoration: none;
-        color: white;
-    }
-
-    .img-thumbnail{
-        border-width: 0px;
     }
 
 </style>
