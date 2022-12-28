67c3a5e48fd117d23459714263f59dee79d899b6
atahf <ataollah@sabanciuniv.edu>
2022-12-28 13:25:59 +0300

    started restyleing editprofile

--- src/components/EditProfile.vue
+++ src/components/EditProfile.vue
@@ -1,9 +1,27 @@
 <template>
     <NavBar></NavBar>
+    <div class="edit-profile-page">
+        <form class="edit-form">
+            <h2 style="color: rgba(17, 73, 158, 0.818)">Edit Profile</h2>
+            <br>
+            <br>
+            <input ref="name" v-model="name" type="text" name="" placeholder="Name" :value="name">
+            <br>
+            <input v-model="mail" type="email" name="" placeholder="Email">
+            <br>
+            <input v-model="gender" type="text" name="" placeholder="Gender">
+            <br>
+            <br>
+            <div class="row-mb-3">
+                <button class="col btn btn-secondary but" @click="goToProfile">CANCEL</button>
+                <button class="col btn btn-danger but" @click="updateUser">SAVE</button>
+            </div>
+        </form>
+    </div>
+</template>
 
+<!-- 
     <div class="container">
-    
-            
         <div class="editp">      
             <div class="info">
                 <h2 style="color: rgba(17, 73, 158, 0.818)">Edit Profile</h2>
@@ -12,7 +30,6 @@
                 <input v-model="gender" type="text" name="" placeholder="Gender">
                 <button @click="goToProfile" style="float:left;">CANCEL</button>
                 <button @click="updateUser" style="float:left; margin-left: 4%;">SAVE</button>
-                
             </div>
             <div class="photo">
                 <img src="../assets/pp.jpg" alt="" class="center">
@@ -21,18 +38,11 @@
                     <input type="file" name="" id="file" accept="image/*">
                     <label for="file">Edit Photo</label>
                     <button class="change"><router-link class= "navborder" to="/ChangePassword">Change Password</router-link></button>
-   
-
                 </div>
-                
-       
             </div>
         </div>
-            
-            
-
     </div>
-</template>
+-->
 
 <script>
     import NavBar from './navbar.vue';
@@ -47,8 +57,27 @@
             return {
                 name: "",
                 mail: "",
-                password: ""
+                password: "",
             };
+        },
+        beforeMount: function() {
+            this.name = this.$el.querySelector('[ref="name"]').value;
+        },
+        mounted() {
+            if(localStorage.userId) this.userId = localStorage.userId;
+
+            const requestOptions = {
+                method: "GET",
+                headers: { "Content-Type": "application/json" },
+            };
+            fetch("https://tfb308.herokuapp.com/api/v1/user/" + this.userId, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                if(data.status === "200") {
+                    this.name = data.returnObject.name;
+                }
+            })
+
         },
         methods: {
             goToProfile() {
@@ -83,159 +112,47 @@
 </script>
 
 <style scoped>
-    
-    head{
-        box-sizing: border-box;
-        padding:0;
-        margin:0;
-        background-color:rgba(47, 20, 109, 0.8);
-    }
-
-    body{
-        margin: 0;
-        height: 100%;
-        align-items: center;
-        justify-content: center;
-        background-color: rgba(47, 20, 109, 0.8);
-        position: absolute;
-        bottom:0px;
-    }
-    h2{
-        margin-left: -70%;
-    }
-    .navborder{
-        color:white;
-        text-decoration: none;
-        font-size: 15px;
-    }
-    
-    .container{
-        background-size: cover;
-        height:100%;
-        width: 100%;
-        align-items: center;
+    .edit-profile-page{
+        margin-top: 70px;
+        padding: 25px 25px 25px 25px;
         display: flex;
-        padding-left: 33%;
-        margin-top: 5%;
-        margin-left: -5%;
+        overflow: hidden;
+        display: flexbox;
+        min-height: calc(100vh - 70px);
+        background: rgba(185, 185, 185, 0.725);
     }
 
-    img{
-        box-sizing: border-box;
-
-        width: 149px;
-        height: 149px;
-        border-radius: 50%;
-        margin-bottom: -50%;
-
-        border:5px solid rgba(31, 28, 101, 0.818);
-        padding:3px;
-        background-color: white;
-        
-    }
-    .editp{
-        background-color: rgb(138, 138, 204);
-        width: 55%;
-        height: 55%;
-        padding: 7%;
-        margin: 1.1em 1em;
-        display: flex;
+    .edit-form{
+        background-color: ghostwhite;
+        width: auto;
+        height: fit-content;
+        padding:30px;
+        margin: auto;
+        display:grid;
         border-radius: 8px;
         text-align: center;
     }
-    .center {
-       
-        margin-left: 60%;
-        margin-top: 5%;
-        margin-bottom: 20%;
-        width: 100%;
+
+    .but{
+        margin: 20px 20px 20px 20px;
+        width: 210px;
     }
 
-    input[type="text"],
-    input[type="password"],
-    input[type="file"],
-    input[type="email"]{
-        display:block;
-        box-sizing: border-box;
-        background: none;
-        margin-bottom: 60px;
-        padding:4px;
-        width:220px;
-        height:32px;
-        border:none;
-        border-bottom: 1px solid;
-        font-weight: 400;
-        font-size: 15px;
-        transition: 0.2;
-
-    }
-    h2{
-        margin-left: -40%;
-        margin-top: 5%;
-        margin-bottom: 20%;
-    }
-
-    input[type="text"]:focus,
-    input[type="password"]:focus,
-    input[type="file"]:focus,
-    input[type="email"]:focus{
-        border-bottom: 2px solid;
-        border-bottom-right-radius: 20px;
-        transition: 0.2 ease;
-    }
-
-    button{
-        border:1px solid;
-        background-color: rgba(17, 73, 158, 0.818);
-        color:white;
-        height: 30px;
-        width: 105px;
-        border-radius: 5px;
-        margin:0px;
-        transition:all 0.3s;
-    }
-
-    button:hover{
+    .but:hover{
         transform: scale(1.1);
-        cursor:pointer;
     }
 
-    input[type="file"]{
-        display:none;
+    .fan-team-but{
+        text-decoration: none;
+        color: black;
+        width: fit-content;
+        margin: auto;
+        padding: 6px;
     }
 
-    label{
-        display:block;
-        margin-left: 70%;
-        margin-right: -60%;
-        margin-top:-30%;
-        font-size: 20px;
-        background-color: rgba(17, 73, 158, 0.818);
-        color:white;
-        border:1px ;
-        padding: 3px 0px;
-        border-radius:5px;
-        transition: all 0.3s;
-    
-      
+    .fan-team-but:hover{
+        background-color: rgba(33, 66, 114, 0.818);
+        color: white;
+        border-radius: 10px;
     }
-    label:hover{
-        transform: scale(1.1);
-        color:white;
-        cursor:pointer;
-    }
-    .editphoto{
-        width:140px;
-        
-    }
-    .change{
-        margin-top: 110%;
-        margin-left: 50%;
-        padding: 3px 1px -3px 1px;
-        height: 40px;
-        width: 180px;
-    }
-
-
-
 </style>
