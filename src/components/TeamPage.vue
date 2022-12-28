7a303eefe041297f28a8775f8669ce6bd3bee3a5
atahf <ataollah@sabanciuniv.edu>
2022-12-28 12:06:39 +0300

    restyled teamPage and fixed fav button

--- src/components/TeamPage.vue
+++ src/components/TeamPage.vue
@@ -4,16 +4,13 @@
         <div class="card team-info-box">
             <div class="row">
                 <div class="col-md-4">
-                    <img :src=team.logoURL class="card-img">
+                    <img :src=team.logoURL class="card-img team-info-box-logo">
                 </div>
                 <div class="col-md-6">
                     <div class="card-body">
-                        <button v-on:click="addToFav" class="card-button">
-                            <i
-                                class="bx"
-                                :class="'bx-heart'"
-                            />
-                        </button>
+                        <svg @click="addToFav" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart fav-button" viewBox="0 0 16 16">
+                            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                        </svg>
                         <h5 class="card-title">{{team.name}}</h5>
                         <br>
                         <p class="card-text">City: {{team.city}}</p>
@@ -236,17 +233,22 @@
         font-style: italic;
     }
 
-    .card-button{
+    .fav-button{
         align-items: right;
         position: absolute;
-        top: 3px;
-        right: 5px;
+        top: 20px;
+        right: 25px;
         border-width: 0px;
-        background-color: white;
-        font-size: 20px;
+        font-size: 50px;
+        height: 30px;
     }
 
-    .card-img{
+    .fav-button:hover{
+        transform: scale(1.5);
+        color: red;
+    }
+
+    .team-info-box-logo{
         border-width: 0px;
         max-height: 200px;
         max-width: 200px;
