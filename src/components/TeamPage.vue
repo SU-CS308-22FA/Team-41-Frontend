1def5a295bb63edd16792e926e296ac6b8bd71bd
atahf <ataollah@sabanciuniv.edu>
2022-12-28 19:16:36 +0300

    bug fixed

--- src/components/TeamPage.vue
+++ src/components/TeamPage.vue
@@ -34,7 +34,7 @@
                 </div>
                 <div class="col-md-6">
                     <div class="card-body">
-                        <svg @click="addToFav" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart fav-button" viewBox="0 0 16 16">
+                        <svg v-if="isLoggedIn()" @click="addToFav" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart fav-button" viewBox="0 0 16 16">
                             <path id="favv" d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
                         </svg>
                         <h5 class="card-title">{{team.name}}</h5>
@@ -144,6 +144,11 @@
             });
         },
         methods: {
+            isLoggedIn(){
+                if(localStorage.isLogedIn === undefined) return false;
+                return true;
+            },
+
             goToMatch(id) {
                 this.$router.push({ name: "MatchPage", params: {matchId: id}});
             },
