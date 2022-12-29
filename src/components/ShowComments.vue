a4f358555dd4e2075ae145abe421dc99160ac6a8
atahf <ataollah@sabanciuniv.edu>
2022-12-29 16:56:17 +0300

    restyled ShowComments

--- src/components/ShowComments.vue
+++ src/components/ShowComments.vue
@@ -1,175 +1,104 @@
 <template>
-  <AdminNavBar></AdminNavBar>
-  <body>
-    <div class="container">
-      <div class="content">
-        <div class="cards">
-          <div v-for="item in items" :key="item.id" class="card">
-            <div class="box">
-              <h2>Reported User Id: {{ item.comment.userId }}</h2>
-              <h3>Reported Content: {{ item.comment.body }}</h3>
-              <h3>Reported Comment Id: {{ item.comment.id }}</h3>
+	<AdminNavBar></AdminNavBar>
+    <div class="reports-page">
+        <div v-if="finishedLoading">
+            <div v-if="items.length > 0" class="row row-cols-2">
+                <div class="col-6 card report-box" v-for="item in items" :key="item.id">
+                    <div class="card-body">
+                        <h4 class="card-title">Username: <b>{{ item.comment.username }}</b></h4>
+                        <h6 class="card-text">Reason: <b>{{ item.type }}</b></h6>
+                        <p class="card-text">Comment: <i>{{ item.comment.body }}</i></p>
+                        <div class="col">
+                            <button class="btn btn-danger but" @click="banUser()">Ban {{ item.comment.username }}</button>
+                            <button class="btn btn-primary but" @click="delRep()">Delete Report</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="icon-case">
-              <button>
-                <img
-                  src="../assets/likee.png"
-                  width="45px"
-                  height="45px"
-                  alt=""
-                />
-              </button>
-              <br /><br /><br />
-              <button>
-                <img
-                  src="../assets/trashh.png"
-                  width="45px"
-                  height="45px"
-                  alt=""
-                />
-              </button>
+            <div v-else class="not-found-report">
+                No Reports Found!
             </div>
-          </div>
         </div>
-      </div>
+        <div v-else>
+            <loadingPage></loadingPage>
+        </div>
     </div>
-  </body>
+
 </template>
 
 <script>
-  import AdminNavBar from "./adminNavbar.vue";
+    import AdminNavBar from "./adminNavbar.vue";
+    import loadingPage from "./loadingPage.vue";
 
-  export default {
-    path: "/ShowComments",
-    name: "ShowComments",
-    components: {
-      AdminNavBar,
-    },
-    data() {
-      return { items: [] };
-    },
-    mounted() {
-      const requestOptions = {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      };
-      fetch("https://tfb308.herokuapp.com/api/v1/report/all", requestOptions)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.status === "200") {
-            this.items = data.returnObject;
-          }
-        });
-    },
-  };
+    export default {
+        path: "/ShowComments",
+        name: "ShowComments",
+        components: {
+            AdminNavBar,
+            loadingPage
+        },
+        data() {
+            return { 
+                items: [],
+                finishedLoading: false,
+            };
+        },
+        mounted() {
+            const requestOptions = {
+                method: "GET",
+                headers: { "Content-Type": "application/json" },
+            };
+            fetch("https://tfb308.herokuapp.com/api/v1/report/all", requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.status === "200") {
+                    this.items = data.returnObject;
+                    this.finishedLoading = true;
+                }
+            });
+        },
+        methods: {
+            banUser() {
+                //TODO
+            },
+
+            delRep() {
+                //TODO
+            },
+        }
+    };
 </script>
 
 <style scoped>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  text-align: left;
-}
+    .reports-page {
+        margin-top: 70px;
+        padding: 25px;
+        min-height: calc(100vh - 70px);
+        background: rgba(185, 185, 185, 0.725);
+    }
 
-body {
-  min-height: 90vh;
-  max-width: 1000vh;
-  background: rgba(185, 185, 185, 1);
-}
-a {
-  text-decoration: none;
-}
-li {
-  list-style: none;
-}
-h2 {
-  color: rgba(218, 38, 152, 0.978);
-  padding-top: 10px;
-  padding-bottom: 10px;
-}
+    .not-found-report{
+        margin-top: 300px;
+        font-weight: bold;
+        font-style: italic;
+        font-size: 35px;
+    }
 
-h3 {
-  color: black(16, 16, 16, 0.532);
-  padding-top: 10px;
-  padding-bottom: 10px;
-}
+    .report-box{
+        width: 46%;
+        margin: 1% 2% 1% 2%;
+        height: 200px;
+    }
 
-.side-menu {
-  position: fixed;
-  background: rgba(31, 28, 101, 0.818);
-  width: 31.5vh;
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  margin-top: 6.5vh;
-  margin-left: -1%;
+    .but{
+        padding: auto;
+        margin: 10px;
+        width: fit-content;
+    }
 
-  flex-direction: column;
-}
-
-.side-menu li {
-  font-size: 24px;
-  padding: 15px 40px;
-  color: white;
-  display: flex;
-  align-items: center;
-}
-.side-menu li:hover {
-  background: white;
-  color: rgba(31, 28, 101, 0.818);
-  cursor: pointer;
-  font-weight: bold;
-}
-.container {
-  position: flex;
-  height: 100%;
-  width: 100%;
-
-  margin-left: 0;
-  background: rgba(185, 185, 185, 0.725);
-
-  overflow-y: auto;
-}
-.container .content {
-  position: relative;
-  margin-top: 5vh;
-}
-
-.container .content .cards {
-  padding: 20px 70px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-wrap: wrap;
-}
-.container .content .cards .card {
-  width: 530px;
-  height: 200px;
-  background: white;
-  margin: 30px 10px;
-  padding: 10px 20px;
-  display: flex;
-
-  justify-content: space-around;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.18);
-}
-.container .content .cards .card .icon-case {
-  padding-top: 20px;
-}
-.container .content .cards .card .box {
-  padding-right: 80px;
-}
-.container .content .cards .card .icon-case img {
-  width: 50px;
-  height: 50px;
-}
-.container .content .cards .card .icon-case img:hover {
-  cursor: pointer;
-  transform: scale(1.1);
-}
-button {
-  background-color: white;
-  border: none;
-}
+    .but:hover{
+        font-weight: bold;
+        font-style: italic;
+        transform: scale(1.1);
+    }
 </style>
