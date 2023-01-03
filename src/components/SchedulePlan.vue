6a9c201f28b10004c0e687a357b3d55b24c87d01
atahf <ataollah@sabanciuniv.edu>
2023-01-03 19:42:38 +0300

    added dynamic scheduling

--- src/components/SchedulePlan.vue
+++ src/components/SchedulePlan.vue
@@ -7,7 +7,7 @@
         <h2>Fixture Generator</h2>
         <br>
         <div class="fix-gen-button">
-            <button><h5> Generate Fixture</h5> </button>
+            <button @click="genFixture()"><h5> Generate Fixture</h5> </button>
         </div>
         <br>
         <h5>Rules</h5>
@@ -21,6 +21,15 @@
         <br>
         <h2>Fixture here</h2>
         <p>Some text..</p>
+        <table>
+            <div v-for="week in genFix" :key="week">
+                <tr v-for="match in week" :key="match">
+                    <td>{{ match.homeTeamName }} vs. {{ match.awayTeamName }}</td>
+                    <td>{{ match.dateAndTime }}</td>
+                    <td>{{ match.stadiumName }}</td>
+                </tr>
+            </div>
+        </table>
     </div>
     </div>
 
@@ -39,6 +48,109 @@
             AdminNavBar,
         
         },
+        data() {
+            return{
+                standings: new Map(),
+                teams: new Map(),
+                genFix: [],
+                ranks: [0, 1, 11, 2, 12, 3, 13, 4, 14, 5, 15, 6, 16, 7, 17, 8, 18, 9, 19, 10],
+                start: new Date(2023, 7, 1),
+            }
+        },
+        mounted() {
+            
+        },
+        methods: {
+            genFixture() {
+                let fix = this.createFixture(this.ranks);
+                let seasonStart = new Date(this.start);
+
+                fetch('https://tfb308.herokuapp.com/api/v1/standings')
+                .then(responsee => responsee.json())
+                .then(ress => {
+                    let lstS = ress.returnObject;
+                    for(let x = 0; x < lstS.length; x++) {
+                        this.standings.set(lstS[x].rank, lstS[x]);
+                    }
+
+                    fetch('https://tfb308.herokuapp.com/api/v1/team/all')
+                    .then(response => response.json())
+                    .then(res => {
+                        let lstT = res.returnObject;
+                        for(let x = 0; x < lstT.length; x++) {
+                            this.teams.set(lstT[x].id, lstT[x]);
+                        }
+
+                        let fixture = [];
+                        for(let i = 0; i < 38; i++) {
+                            let week = [];
+                            let weekStart = new Date(seasonStart);
+                            for(let j = 0; j <= 3; j++) {
+                                weekStart.setHours(16)
+                                for(let k = 1; k <= 3; k++) {
+                                if(fix[i][j*k][0] !== 0 && fix[i][j*k][1] !== 0) {
+                                    var home = this.teams.get(this.standings.get(fix[i][j*k][0]).teamId);
+                                    var away = this.teams.get(this.standings.get(fix[i][j*k][1]).teamId);
+                                    week.push(this.matchTemplate(home, away, weekStart.toString()));
+                                    weekStart.setHours(weekStart.getHours()+2);
+                                }
+                                }
+                                weekStart.setDate(weekStart.getDate()+2);
+                            }
+                            seasonStart.setDate(seasonStart.getDate() + 7);
+                            fixture.push(week);
+                        }
+                        this.genFix = fixture;
+                    });
+                });
+            },
+            createFixture(teams) {
+                const count = teams.length;
+                const sets = count - 1;
+                const half = count / 2;
+                const firstHalf = [];
+                for (let turn = 0; turn < sets; turn++) {
+                    const pairings = [];
+                    for (let i = 0; i < half; i++) {
+                    pairings.push([teams[i], teams[count - i - 1]]);
+                    }
+                    teams.splice(1, 0, teams.pop());
+                    firstHalf.push(pairings);
+                }
+                const fixture = [];
+                for (let i = 0; i < 10; i++) {
+                    fixture.push(firstHalf[i]);
+                    if (i !== 9) {
+                    fixture.push(firstHalf[i + 10]);
+                    }
+                }
+                const secondHalf = [];
+                for (const f of fixture) {
+                    const tmp = [];
+                    for (const m of f) {
+                    const tmpList = [m[1], m[0]];
+                    tmp.push(tmpList);
+                    }
+                    secondHalf.push(tmp);
+                }
+                secondHalf.reverse();
+                for (const f of secondHalf) {
+                    fixture.push(f);
+                }
+                return fixture;
+            },
+
+            matchTemplate(home, away, d) {
+                return {
+                    homeTeamName: home.name,
+                    awayTeamName: away.name,
+                    referee: "",
+                    city: home.city,
+                    stadiumName: home.stadiumName,
+                    dateAndTime: d
+                };
+            },
+        }
 
     };
 </script>
